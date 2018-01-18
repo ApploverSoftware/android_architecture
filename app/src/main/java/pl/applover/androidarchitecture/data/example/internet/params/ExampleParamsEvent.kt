e9ae5a5d12d3e655539a27d65ce4e73388c0755c
Janusz <januszhain@wp.pl>
2018-01-18 09:09:16 +0100

    Refactoring

--- app/src/main/java/pl/applover/androidarchitecture/data/example/internet/params/ExampleParamsEvent.kt
+++ app/src/main/java/pl/applover/androidarchitecture/data/example/internet/params/ExampleParamsEvent.kt
@@ -8,7 +8,7 @@ import pl.applover.androidarchitecture.data.internet.params.Params
 /**
  * Created by Janusz Hain on 2018-01-12.
  */
-data class ExampleParamsEvent(private val exampleParamsEventModel: ExampleParamsEventModel) : Params {
+data class ExampleParamsEvent(private val paramsBody: ParamsBody) : Params {
 
     /**
      * If you need json string then:
@@ -19,16 +19,18 @@ data class ExampleParamsEvent(private val exampleParamsEventModel: ExampleParams
      */
     override fun createJsonString(): String {
         val moshi = Moshi.Builder().build()
-        val jsonAdapter = moshi.adapter(ExampleParamsEventModel::class.java)
-        return jsonAdapter.toJson(exampleParamsEventModel)
+        val jsonAdapter = moshi.adapter(ParamsBody::class.java)
+        return jsonAdapter.toJson(paramsBody)
     }
+
+    data class ParamsBody(
+            @Json(name = "event") val event: Event
+    )
+
+    data class Event(
+            @Json(name = "to") val to: String,
+            @Json(name = "from") val from: String
+    )
+
 }
 
-data class ExampleParamsEventModel(
-        @Json(name = "event") val event: ExampleParamsEventDetailsModel
-)
-
-data class ExampleParamsEventDetailsModel(
-        @Json(name = "to") val to: String,
-        @Json(name = "from") val from: String
-)
